aaad34c17d0aa74104572a98a34702e15f883b81
Manos <manosnikitakis3@gmail.com>
2025-10-17 19:07:33 +0300

    Add root file with all components

---
+++ app/(root)/page.tsx
@@ -0,0 +1,16 @@
+import Hero from "@/components/Hero"
+import Navbar from "@/components/Navbar"
+import Footer from "@/components/Footer"
+
+const page = () => {
+  return (
+     <div>  
+        <Navbar />
+        <Hero />
+        <Footer />
+    </div>
+    
+  )
+}
+
+export default page
