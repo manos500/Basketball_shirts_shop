afebbaaff8b90037cb574bf8d0fe9fcce10e9462
Manos <manosnikitakis3@gmail.com>
2025-10-20 10:49:17 +0300

    Add socials login component

---
+++ components/SocialProviders.tsx
@@ -0,0 +1,83 @@
+"use client"
+
+import Image from "next/image";
+import { useState } from "react";
+import { signIn } from "@/lib/auth-client";
+
+type Props = {
+  variant?: "sign-in" | "sign-up";
+  setIsPending?: (value: boolean) => void;
+};
+
+export const SocialProviders = ({variant, setIsPending }: Props) => {
+  
+  const handleGoogleSignIn = async () => {
+    if (setIsPending){
+      setIsPending (true);
+    }
+    
+
+    try {
+      await signIn.social({
+        provider: "google",
+        callbackURL: "/",
+        errorCallbackURL: "/sign-in",
+      });
+
+
+      console.log("Redirecting to Google...");
+    } catch (error: any) {
+      console.error("Google sign-in failed:", error);
+      alert("Google sign-in failed: " + error?.message || error);
+    } finally {
+      if (setIsPending) setIsPending(false);
+    }
+  };
+
+  const handleFacebookSignIn = async () => {
+    if (setIsPending){
+      setIsPending (true);
+    }
+    
+
+    try {
+      await signIn.social({
+        provider: "facebook",
+        callbackURL: "/",
+        errorCallbackURL: "/sign-in",
+      });
+
+
+      console.log("Redirecting to Facebook...");
+    } catch (error: any) {
+      console.error("Facebook sign-in failed:", error);
+      alert("Facebook sign-in failed: " + error?.message || error);
+    } finally {
+      if (setIsPending) setIsPending(false);
+    }
+  };
+
+  return (
+    <div className="py-3">
+        <button
+        type="button"
+        className="flex w-full items-center justify-center gap-3 rounded-full  bg-light-100 dark:bg-dark px-4 py-3 text-body-medium text-dark-900 dark:text-text-light-muted dark:hover:bg-dark-light dark:hover:text-text-light  hover:bg-light-ultra-dark hover:outline-none cursor-pointer"
+        aria-label={`${variant === "sign-in" ? "Continue" : "Sign up"} with Google`}
+        onClick={handleGoogleSignIn}
+      >
+        <Image src="/google.png" alt="" width={20} height={20} />
+        <span>Continue with Google</span>
+      </button>
+      
+        <button
+        type="button"
+        className="flex w-full items-center justify-center gap-3 rounded-full  bg-light-100 dark:bg-dark px-4 py-3 text-body-medium text-dark-900 dark:text-text-light-muted dark:hover:bg-dark-light dark:hover:text-text-light  hover:bg-light-ultra-dark hover:outline-none cursor-pointer"
+        aria-label={`${variant === "sign-in" ? "Continue" : "Sign up"} with Google`}
+        onClick={handleFacebookSignIn}
+      >
+        <Image src="/facebook.png" alt="" width={20} height={20} />
+        <span>Continue with Facebook</span>
+      </button>
+    </div>
+  )
+}
