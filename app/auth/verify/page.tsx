0edd29e6b860dcc51d1bcf5e67a0048a3a5ba073
Manos <manosnikitakis3@gmail.com>
2025-10-24 14:48:07 +0300

    Add auth/verify route to validate email verification

---
+++ app/auth/verify/page.tsx
@@ -0,0 +1,26 @@
+import SendVerificationEmailForm from "@/components/SendVerificationEmailForm";
+import { redirect } from "next/navigation";
+
+
+interface PageProps {
+    searchParams: Promise<{error: string}>;
+}
+
+const page = async ({searchParams}: PageProps) => {
+    const error = (await searchParams).error;
+
+    if (!error) redirect("/")
+  return (
+
+    <div className='flex items-center justify-center space-y-4 flex-col h-[70vh]'>
+        <h1 className='text-heading-3'>Verify Email</h1>
+        <p className='text-destructive'>
+            {error === "invalid_token" || error === "token_expired" ? "Your token is invalid or expired. Please request a new one." : "Oops! Something went wrong. Please try again."}
+        </p>
+        <SendVerificationEmailForm/>
+    </div>
+
+  )
+}
+
+export default page
