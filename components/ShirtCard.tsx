190cd929cc8a71a3b289255d829b3f9bf900ddfc
Manos <manosnikitakis3@gmail.com>
2025-11-30 11:21:55 +0200

    add component for displaying shirts

---
+++ components/ShirtCard.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Card, CardContent } from "@/components/ui/card";
+import { Shirt } from "@/lib/type";
+
+interface ShirtCardProps {
+  shirt: Shirt;
+}
+
+export default function ShirtCard({ shirt }: ShirtCardProps) {
+ 
+  return (
+    <Card className="w-full h-[520px] flex flex-col justify-between overflow-hidden shadow-lg bg-light-light rounded-lg">
+      <CardContent className="flex flex-col justify-between h-full p-4">
+        <Link
+          href={`/shirts/${shirt.id}`}
+          className="block group cursor-pointer flex-1"
+        >
+          <div className="relative w-full h-80 mb-3 rounded-lg overflow-hidden">
+            <Image
+              src={`/${shirt.mainImage}`}
+              alt={shirt.name}
+              fill
+              className="object-contain group-hover:scale-105 transition-transform duration-300 ease-in-out"
+            />
+          </div>
+
+          <h3 className="text-lg font-medium text-dark-900 transition-colors truncate">
+            {shirt.name}
+          </h3>
+
+          {shirt.description && (
+            <p className="text-sm text-gray-600 line-clamp-2">
+              {shirt.description}
+            </p>
+          )}
+        </Link>
+
+        <div className="flex gap-2 items-center mt-3">
+          {shirt.minPrice !== undefined && (
+            <span className="text-text-dark font-medium text-lg">
+              ${shirt.minPrice.toFixed(2)}
+            </span>
+          )}
+          {shirt.maxPrice !== undefined && shirt.maxPrice !== shirt.minPrice && (
+            <span className="line-through">
+              ${shirt.maxPrice.toFixed(2)}
+            </span>
+          )}
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
