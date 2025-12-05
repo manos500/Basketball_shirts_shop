85a3d33511eab90cb9e67f6093fb81a218bfedb8
Manos <manosnikitakis3@gmail.com>
2025-12-05 20:23:43 +0200

    add QueryClientProvider in layout

---
+++ app/(root)/shirts/layout.tsx
@@ -0,0 +1,19 @@
+// app/layout.tsx
+"use client";
+
+import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </body>
+    </html>
+  );
+}
