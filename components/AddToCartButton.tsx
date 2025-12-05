f7e955d7c9e041a4dda0036a6f62883095ee7b7f
Manos <manosnikitakis3@gmail.com>
2025-12-05 20:22:20 +0200

    add button for adding shirts to cart

---
+++ components/AddToCartButton.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { useAddToCart } from "@/lib/hooks/useAddToCart";
+import { useSession } from "@/lib/auth-client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+interface AddToCartButtonProps {
+  shirtName: string;
+  variantId: string;
+  quantity?: number;
+}
+
+export default function AddToCartButton({
+  shirtName,
+  variantId,
+  quantity = 1,
+}: AddToCartButtonProps) {
+  const { data: session } = useSession();
+  const userId = session?.user?.id;
+
+  const router = useRouter();
+
+  const { mutate: addToCart, isPending } = useAddToCart();
+
+  const handleClick = () => {
+    addToCart(
+      { userId, variantId, quantity },
+      {
+        onSuccess: () => {
+          router.push("/cart");
+        },
+      }
+    );
+  };
+
+  return (
+    <button
+      onClick={handleClick}
+      disabled={isPending}
+      className="flex items-center justify-center gap-2 rounded-full bg-dark px-6 py-4 text-body-medium text-text-light transition hover:opacity-90 hover:cursor-pointer disabled:opacity-50"
+    >
+      <Image
+        src={`/logos/nba/LosAngelesLakers.png`}
+        alt={shirtName}
+        height={35}
+        width={35}
+        priority
+      />
+
+      {isPending ? (
+        <div className="flex justify-center items-center gap-2">
+          <div className="loader_circle" />
+        </div>
+      ) : (
+        "Add to Cart"
+      )}
+    </button>
+  );
+}
