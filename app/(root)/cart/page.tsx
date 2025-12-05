77dc9dde46f02da4d265c36ee729eac631bb9a0c
Manos <manosnikitakis3@gmail.com>
2025-12-05 20:16:32 +0200

    add cart page

---
+++ app/(root)/cart/page.tsx
@@ -0,0 +1,14 @@
+import Cart from "@/components/Cart";
+
+
+const CartPage = async () => {
+
+
+  return (
+    <div className="mx-auto container pt-20 lg:pt-30 pb-12 px-4 sm:px-6 lg:px-8 min-h-screen">
+      <Cart  />
+    </div>
+  );
+};
+
+export default CartPage;
