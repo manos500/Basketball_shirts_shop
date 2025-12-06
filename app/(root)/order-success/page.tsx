f8fb66c5b6b15e0aa063aa316baf501327454bad
Manos <manosnikitakis3@gmail.com>
2025-12-06 19:04:22 +0200

    add page for successfully order purchase

---
+++ app/(root)/order-success/page.tsx
@@ -0,0 +1,17 @@
+export default function OrderSuccess({
+  searchParams,
+}: {
+  searchParams: { orderId: string };
+}) {
+  return (
+    <div className="mx-auto container pt-20 lg:pt-30 pb-12 px-4 sm:px-6 lg:px-8 min-h-screen bg-light">
+        <div className="max-w-xl mx-auto py-24 text-center">
+        <h1 className="text-3xl font-bold mb-4">✅ Order Completed</h1>
+        <p>Your payment was successful.</p>
+        <p className="mt-4 font-semibold">
+            Order ID: {searchParams.orderId}
+        </p>
+        </div>
+    </div>
+  );
+}
