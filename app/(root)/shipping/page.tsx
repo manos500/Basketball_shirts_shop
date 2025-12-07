dd6df28dc08dd3aa7e504d2f3e59db62cb0aeedb
Manos <manosnikitakis3@gmail.com>
2025-12-07 12:28:12 +0200

    Changed shadows

--- app/(root)/shipping/page.tsx
+++ app/(root)/shipping/page.tsx
@@ -3,8 +3,8 @@ import Shipping from '@/components/Shipping'
 
 const page = () => {
   return (
-    <div className=" flex justify-center pt-20 lg:pt-30">
-         <div className="w-full md:w-[60vw] xl:w-[35vw] bg-light-light rounded-lg p-8 shadow-shadow-m text-center space-y-6">
+    <div className=" flex justify-center pt-20 lg:pt-30 min-h-screen">
+         <div className="w-full md:w-[60vw] xl:w-[35vw] bg-light-dark rounded-lg p-8 text-center space-y-6">
             <Shipping/>
          </div>
     </div>
