fca10789ea4a0c74f5d8336af44f3511f51d2192
Manos <manosnikitakis3@gmail.com>
2026-01-04 13:02:29 +0200

    Modified colors

--- components/CollapsibleSection.tsx
+++ components/CollapsibleSection.tsx
@@ -22,7 +22,7 @@ const CollapsibleSection = ({
     console.log(open)
 
   return (
-    <div className={`${className} shadow-lg bg-light-light`}>
+    <div className={`${className} bg-light-light`}>
         <button aria-expanded={open} className="flex w-full items-center justify-between gap-4 text-left focus:outline-none focus-visible:ring-2 focus-visible:ring-[--color-dark-500] cursor-pointer " onClick={() => {setOpen((o) => !o)}}>
            <span className="text-xl font-medium text-dark">{title}</span> 
            <Image src={(open) ? "/minusSymbol.png" : "/plusSymbol.png"} alt="symbol" width={12} height={12} priority></Image>
