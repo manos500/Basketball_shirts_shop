78d9320ed296eb922fe4b4642ad6dff8fbb47132
Manos <manosnikitakis3@gmail.com>
2026-01-04 18:09:33 +0200

    Modified colors

--- app/(root)/shipping/page.tsx
+++ app/(root)/shipping/page.tsx
@@ -1,10 +1,9 @@
 import Shipping from '@/components/Shipping'
 
-
 const page = () => {
   return (
-    <div className=" flex justify-center pt-20 lg:pt-30 min-h-screen">
-         <div className="w-full md:w-[60vw] xl:w-[35vw] bg-light-dark rounded-lg p-8 text-center space-y-6">
+    <div className="flex justify-center pt-20 lg:pt-30 min-h-screen">
+         <div className="w-full md:w-[60vw] xl:w-[35vw] bg-light-light rounded-lg p-8 text-center space-y-6">
             <Shipping/>
          </div>
     </div>
