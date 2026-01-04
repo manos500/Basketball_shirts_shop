77f5e070310f8ddad534bd6f34fbd93dd29cccee
Manos <manosnikitakis3@gmail.com>
2026-01-04 18:08:31 +0200

    Added nba and euroleague teams

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,5 @@
 import Hero from "@/components/Hero"
-import Shirts from "@/components/Shirts"
+import { getEuroleagueTeams, getNBATeams } from "@/lib/actions/teams"
 import Featured from "@/components/Featured"
 
 interface PageProps {
@@ -7,11 +7,20 @@ interface PageProps {
 }
 
 const page = async ({searchParams}: PageProps) => {
+  const nbaTeams = await getNBATeams();
+  const euroleagueTeams = await getEuroleagueTeams();
   return (
-     <div>   
+     <div className="bg-light-light">   
         <Hero />
-        <Featured />
-        <Shirts searchParams = {searchParams}/>    
+        <div className="flex justify-center pt-10">
+          <h1 className="text-heading-2">NBA Teams</h1>
+        </div>
+        <Featured items={nbaTeams} type="team"/>  
+        <div className="flex justify-center mx-auto sm:ml-0 pt-10">
+          <h1 className="text-heading-2 mx-auto">Euroleague Teams</h1>
+        </div>
+        <Featured items={euroleagueTeams} type="team"/>  
+
     </div>
     
   )
