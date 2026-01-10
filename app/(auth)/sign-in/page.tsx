b905abf2d58ee05ad88140c6e76e97e71bb9f8f5
Manos <manosnikitakis3@gmail.com>
2026-01-10 11:07:39 +0200

    Moved files in auth folder

--- app/(auth)/sign-in/page.tsx
+++
@@ -1,6 +0,0 @@
-import AuthForm from "@/components/AuthForm"
-const page = () => {
-  return <AuthForm mode="sign-in"/>
-}
-
-export default page
--- app/(auth)/sign-up/page.tsx
+++
@@ -1,6 +0,0 @@
-import AuthForm from "@/components/AuthForm"
-const page = () => {
-  return <AuthForm mode="sign-up"/>
-}
-
-export default page
