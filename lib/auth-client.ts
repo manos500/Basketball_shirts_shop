b0c98d0ddbe8c2766a7712d2939e5bd021878c17
Manos <manosnikitakis3@gmail.com>
2026-01-20 14:28:14 +0200

    added better-auth admin plugin

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,12 @@
-import { createAuthClient } from "better-auth/react"
+import { createAuthClient } from "better-auth/react";
+import {inferAdditionalFields, adminClient} from "better-auth/client/plugins";
+import type { auth } from "@/lib/auth"
+import { roles, ac } from "./permissions";
+
 
 const authClient = createAuthClient({
-    baseURL: process.env.NEXT_PUBLIC_API_UR
-})
+    baseURL: process.env.NEXT_PUBLIC_API_UR,
+    plugins: [inferAdditionalFields<typeof auth>(), adminClient({ ac, roles })],
+});
 
-export const { signUp, signOut, signIn, useSession, sendVerificationEmail, forgetPassword, resetPassword} = authClient;
+export const { signUp, signOut, signIn, useSession, sendVerificationEmail, forgetPassword, resetPassword, admin } = authClient;
