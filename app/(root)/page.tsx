274ddba818df662f3f604161e9b5995736253dc3
Manos <manosnikitakis3@gmail.com>
2026-01-20 14:00:59 +0200

    Added animation for heading

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/Hero"
 import { getEuroleagueTeams, getNBATeams } from "@/lib/actions/teams"
 import Featured from "@/components/Featured"
+import AnimatedHeading from "@/components/AnimatedHeading"
 
 interface PageProps {
   searchParams: { [key: string]: string | undefined }
@@ -9,20 +10,24 @@ interface PageProps {
 const page = async ({searchParams}: PageProps) => {
   const nbaTeams = await getNBATeams();
   const euroleagueTeams = await getEuroleagueTeams();
-  return (
-     <div className="bg-light-light">   
-        <Hero />
-        <div className="flex justify-center pt-10">
-          <h1 className="text-heading-2">NBA Teams</h1>
-        </div>
-        <Featured items={nbaTeams} type="team"/>  
-        <div className="flex justify-center mx-auto sm:ml-0 pt-10">
-          <h1 className="text-heading-2 mx-auto">Euroleague Teams</h1>
-        </div>
-        <Featured items={euroleagueTeams} type="team"/>  
 
+  return (
+    <div className="bg-light-light">   
+      <Hero />
+      <div className="absolute top-223" id="teams"></div>
+      <div className="flex justify-center pt-10">
+        <AnimatedHeading>
+          <h1 className="text-heading-2">NBA Teams</h1>
+        </AnimatedHeading>
+      </div>
+      <Featured items={nbaTeams} type="team"/>  
+      <div className="flex justify-center mx-auto sm:ml-0">
+        <AnimatedHeading>
+          <h1 className="text-heading-2 mx-auto">Euroleague Teams</h1>
+        </AnimatedHeading>
+      </div>
+      <Featured items={euroleagueTeams} type="team"/>  
     </div>
-    
   )
 }
 
